77426548f91f07a7a0d3240c5d6b34f42056304e
waltertaya <waltermittybecker5@gmail.com>
2023-12-18 22:42:43 +0300

    bubble sort updated betty style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,26 +10,21 @@
 
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j;
-int tmp;
+    size_t i, j;
+    int tmp;
 
-if (array == NULL || size < 2)
-{
-return (1);
-}
-for (i = 0; i < size - 1; i++)
-{
-for (j = 0; j < size - 1; j++)
-{
-if (array[j] > array[j + 1])
-{
-tmp = array[j + 1];
-array[j + 1] = array[j];
-array[j] = tmp;
+    if (array == NULL || size < 2)
+        return;
 
-print_array(array, size);
+    for (i = 0; i < size - 1; i++)
+        for (j = 0; j < size - 1; j++)
+            if (array[j] > array[j + 1])
+            {
+                tmp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = tmp;
 
-}
-}
-}
+                print_array(array, size);
+
+            }
 }

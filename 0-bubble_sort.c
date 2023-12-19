e9bf1976b00e11a76970040d25f94cdeb17abffd
waltertaya <waltermittybecker5@gmail.com>
2023-12-19 12:39:22 +0300

    requirement check

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,4 @@
-# include "sort.h"
+#include "sort.h"
 
 /**
 *bubble_sort - sorts an array of integers in ascending order
@@ -9,27 +9,27 @@
 
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j;
-int tmp;
+	size_t i, j;
+	int tmp;
 
-if (array == NULL || size < 2)
-{
-return;
-}
+	if (array == NULL || size < 2)
+	{
+		return (1);
+	}
 
-for (i = 0; i < size - 1; i++)
-{
-for (j = 0; j < size - 1; j++)
-{
-if (array[j] > array[j + 1])
-{
-tmp = array[j + 1];
-array[j + 1] = array[j];
-array[j] = tmp;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = tmp;
 
-print_array(array, size);
+				print_array(array, size);
 
-}
-}
-}
+			}
+		}
+	}
 }
